2c0e650b1e81cbc603ed9a8a1f20deed7cb8f93f
cainafigueiredo <cainafigueiredo@poli.ufrj.br>
2021-05-12 18:23:22 -0300

    Advanced JSX completed

---
+++ Advanced JSX/app.js
@@ -0,0 +1,221 @@
+// Class vs className
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myDiv = (
+  <div className="big">
+    I AM A BIG DIV
+  </div>
+);
+
+ReactDOM.render(
+  myDiv,
+  document.getElementById('app')
+);
+
+// Self-closing tags
+const profile = (
+	<div>
+	  <h1>I AM JENKINS</h1>
+	  <img src="images/jenkins.png" />
+	  <article>
+		I LIKE TO SIT
+		<br />
+		JENKINS IS MY NAME
+		<br />
+		THANKS HA LOT
+	  </article>
+	</div>
+);
+
+// JavaScript In Your JSX In Your JavaScript
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1>2 + 3</h1>,
+  document.getElementById('app')
+);
+
+// Curly Braces in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1>{2 + 3}</h1>,
+  document.getElementById('app')
+);
+
+// 20 Digits of Pi in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const math = (
+  <h1>2 + 3 = {2+3}</h1>
+);
+
+ReactDOM.render(
+  math,
+  document.getElementById('app')
+);
+
+// Variables in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const theBestString = 'tralalalala i am da best';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
+
+// Variable Attributes in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jpg';
+
+// Declare new variable here:
+const gooseImg = <img src={goose} />;
+
+ReactDOM.render(
+  gooseImg,
+  document.getElementById('app')
+);
+
+// Event Listeners in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" 
+    onClick={makeDoggy}/>
+);
+
+ReactDOM.render(
+  kitty,
+  document.getElementById('app')
+);
+
+// JSX Conditionals: If Statements That Don't Work
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() == 'heads') {
+  img = <img src={pics.kitty} />;
+} else {
+  img = <img src={pics.doggy} />;
+}
+
+ReactDOM.render(
+  img,
+  document.getElementById('app')
+);
+
+// JSX Conditionals: The Ternary Operator
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img 
+  src={
+    pics[coinToss() == 'heads' ? 'kitty' : 'doggy']
+  } />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
+
+// JSX Conditionals: &&
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+	favoriteFoods, 
+	document.getElementById('app')
+);
+
+// .map in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <ul>{peopleLis}</ul>,
+  document.getElementById('app') 
+);
+
+// Keys
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map((person,i) =>
+  // expression goes here:
+  <li key={'person_' + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <ul>{peopleLis}</ul>,
+  document.getElementById('app') 
+);
+
+// React.createElement
+const greatestDivEver = React.createElement("div", null, "i am div");
