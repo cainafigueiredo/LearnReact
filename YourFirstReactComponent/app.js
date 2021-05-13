02714d8f700bc859727ff2dd09c05ebf9501d29a
cainafigueiredo <cainafigueiredo@poli.ufrj.br>
2021-05-13 14:28:02 -0300

    Your First React Component was completed.

---
+++ YourFirstReactComponent/app.js
@@ -0,0 +1,75 @@
+// React components: small reusable chunks of code
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+};
+
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
+
+// Import React
+import React from 'react';
+
+// Import ReactDOM
+import ReactDOM from 'react-dom';
+
+// Create a Component Class
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class x extends React.Component {
+
+};
+
+// Name a Component Class
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+
+};
+
+// The Render Function
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+};
+
+// Create a Component Instance
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+// component goes here:
+<MyComponentClass />
+
+// Render A Component
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+// component goes here:
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
