7b3db2dcaf75585164158dae67277a4f44e91149
Cainã Figueiredo Pereira <41930269+cainafigueiredo@users.noreply.github.com>
2021-05-21 16:20:18 -0300

    This.props partialy completed.

---
+++ Props/app.js
@@ -0,0 +1,88 @@
+// Access a Component's props
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <PropsDisplayer />,
+  document.getElementById('app')
+);
+
+// Pass `props` to a Component
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <PropsDisplayer myProp="Hello"/>,
+  document.getElementById('app')
+);
+
+// Render a Component's props
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting firstName='Cainã' />, 
+  document.getElementById('app')
+);
+
+// Pass props From Component To Component
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Greeting} from "./Greeting.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hullo and, "Welcome to The Newzz," "On Line!"
+        </h1>
+        <Greeting name="Cainã" />
+        <article>
+          Latest newzz:  where is my phone?
+        </article>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+);
+
